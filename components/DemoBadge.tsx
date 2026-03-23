4698abb7c5283f42ddaa934fa429a55a4295b2cf
MarcoA366 <marco_95_5@yahoo.com>
2026-03-23 16:43:55 -0300

    md

--- components/DemoBadge.tsx
+++ components/DemoBadge.tsx
@@ -1,7 +1,7 @@
 export default function DemoBadge() {
   return (
     <div className="text-center fixed bottom-4 right-4 bg-black/70 text-white text-sm px-4 py-2 rounded-lg shadow-lg z-50 pointer-events-auto">
-      🌐 Este es un sitio de ejemplo de <br/> {" "}
+       🌐 Sitio de ejemplo:{" "}
       <a
         href="https://zonaweb.site"
         target="_blank"
